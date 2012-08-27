04aa6ec3d36e83b0e9139bbbb55c853f8955bfce
Firstname Lastname <gjero@krsteski.de>
2012-08-27 10:48:43 +0200

    add class docs

--- src/Columns.php
+++ src/Columns.php
@@ -1,10 +1,32 @@
 <?php
 /**
- * ExtJsTypes_Columns
+ * PHP Version 5
  *
+ * The ExtJS-Types-Builder is a OOP-PHP-Framework
+ * and creates JSON suitable strings for the communication
+ * with your ExtJS-GUI.
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * http://krsteski.de/new-bsd-license/
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to gjero@krsteski.de so we can send you a copy immediately.
+ *
+ * @copyright Copyright (c) 2010-2011 Gjero Krsteski (http://krsteski.de)
+ * @license   http://krsteski.de/new-bsd-license New BSD License
+ */
+
+/**
  * This class specifies the definition for a column inside a Ext.grid.Panel.
  * It encompasses both the grid header configuration as well as displaying
  * data within the grid itself.
+ *
+ * @package ExtJsTypes
+ * @author Gjero Krsteski <gjero@krsteski.de>
  */
 class ExtJsTypes_Columns extends ExtJsTypes_TypeAbstract
 {
